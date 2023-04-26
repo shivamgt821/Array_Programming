60f338d1a32569a58b184cb9f939cdca73cee614
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-04-26 11:07:47 +0530

    Create JaggedArray.java

---
+++ JaggedArray.java
@@ -0,0 +1,13 @@
+public class JaggedArray {
+
+	public static void main(String[] args) {
+		int [][]a = {{1,2,3},{1,4,3,2,5},{1},{2,3,4,5}};
+		for(int i=0; i<a.length; i++) {
+			for(int j=0; j<a[i].length; j++) {
+				System.out.print(a[i][j]+ " ");
+			}
+			System.out.println();
+		}
+		
+	}
+}
