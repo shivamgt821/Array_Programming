41a373be671dac61ac15a7d876cc21f50027de2e
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-04-27 23:26:05 +0530

    Create Sum_of_each_row.java

---
+++ Sum_of_each_row.java
@@ -0,0 +1,19 @@
+public class Sum_of_each_row {
+
+	public static void main(String[] args) {
+		int a[][] = {
+				{1,2,3},
+				{4,5,6},
+				{7,8,9}
+			};
+		for(int i=0;i<a.length; i++) {
+			int sum = 0;
+			for(int j=0;j<a[i].length; j++) {
+				sum+=a[i][j];
+			}
+		
+		System.out.println("the sum of "+(i+1)+" row is : " + sum);
+		}
+	}
+}
+
