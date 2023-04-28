5a65d816d379d028872eee4c79ae76a9f3fc19de
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-04-28 21:46:41 +0530

    Create ReverseOFArray.java

---
+++ ReverseOFArray.java
@@ -0,0 +1,9 @@
+public class ReverseOFArray {
+
+	public static void main(String[] args) {
+		int a[] = {3,6,9,1,4,8};
+		for(int i=a.length-1; i>=0; i--){
+			System.out.print(a[i]+" ");
+		}
+	}
+}
